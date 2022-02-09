ad95c363c6b65e51c2a5958504993c04005fb2dd
Undeadworker <jgino@csumb.edu>
2022-02-08 19:05:02 -0800

    lab 4

---
+++ lab4/js/lab.js
@@ -0,0 +1,14 @@
+// how to make a basic platformer.
+// move around and jump too
+// @link   URL
+// @file   This files defines the MyClass class.
+// @Jason MF Gino
+// @since  2.8.2022
+//task 4
+// how to make a basic platformer
+// 1. draw a player sprite
+// 2. give the sprite a left right control
+// 3. add a jump
+// 3.1. add gravity if needed
+// 4.add platforms to jump too.
+// 5. add a end point
