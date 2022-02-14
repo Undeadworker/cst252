3c95d82e11d6fce14872918b5915a6fdf5ac0624
Undeadworker <jgino@csumb.edu>
2022-02-14 11:43:15 -0800

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,31 @@
+/**
+ * Author:    Jason Matthew F. Gino
+ * Created:   2.13.2022
+ * (c) Copyright by Ginoworks.
+ **/
+
+
+// Given
+make = "Ford";
+
+model = "Super Cool One";
+
+color = "Bad yellow";
+
+year = 2020
+
+
+//Input
+
+
+//Math
+age = 2022 - year;
+
+//output
+document.writeln("Make: " + make + "<br>" );
+
+document.writeln("Model: " + model + "<br>");
+
+document.writeln("Color: " + color + "<br>");
+
+document.writeln("Year: " + age + "Years olD<br>")
