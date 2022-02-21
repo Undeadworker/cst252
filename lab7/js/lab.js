9615ea2cad9a511acbbff94d5b7f2d6bfa02141f
Undeadworker <jgino@csumb.edu>
2022-02-21 11:24:19 -0800

    lab7pre

---
+++ lab7/js/lab.js
@@ -0,0 +1,18 @@
+/**
+ * Author:    Jason Matthew F. Gino
+ * Created:   2.16.2022
+ * (c) Copyright by Ginoworks.
+ **/
+
+
+// Given
+if (person != null) {
+  document.getElementById("demo").innerHTML =
+  "Hello " + person + "! How are you today?";
+}
+//Input
+let person = prompt("Please enter your name", "Harry Potter");
+
+//Math
+
+//output
